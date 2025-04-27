6566a2b0cb1f99f827e7d6a822bb0da0398eaa05
ignabarolo <baroloignacio@gmail.com>
2025-04-26 22:04:32 -0300

    feat:IB: Creacion e inicializacion del proyecto

---
+++ src/test/java/com/utn/buensaborApi/BuensaborApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.buensaborApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuensaborApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
