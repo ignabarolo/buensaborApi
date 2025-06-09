fce609d3f7e1b43786ce66533b1a7c5dbbb51ed3
Ignacio Barolo <baroloignacio@gmail.com>
2025-06-09 19:31:14 -0300

    Ser agrega anotacions Json para el manejo de Articulos en pedido

---
+++ src/main/java/com/utn/buensaborApi/models/Dtos/Pedido/PedidoVentaDto.java
@@ -0,0 +1,53 @@
+package com.utn.buensaborApi.models.Dtos.Pedido;
+
+import com.utn.buensaborApi.enums.Estado;
+import com.utn.buensaborApi.enums.FormaPago;
+import com.utn.buensaborApi.enums.TipoEnvio;
+import com.utn.buensaborApi.models.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PedidoVentaDto extends BaseEntity {
+    private LocalDate fechaPedido;
+    private LocalTime horaPedido;
+    private Estado estado;
+    private TipoEnvio tipoEnvio;
+    private Double gastoEnvio;
+    private FormaPago formaPago;
+    private Double descuento;
+    private Double totalCosto;
+    private Double totalVenta;
+
+
+    private DomicilioDto domicilio;
+
+    private Set<PedidoVentaDetalleDto> pedidosVentaDetalle;
+
+//    private Cliente cliente;
+
+//    private Empleado empleado;
+
+    public LocalTime HoraFinalizacion() {
+        return this.horaPedido;
+    }
+
+    public Double DescuentosCalculados() {
+        return 0D;
+    }
+
+    public Double TotalCalculado() {
+        return 0D;
+    }
+
+    public Double TotalCosto() {
+        return 0D;
+    }
+}
