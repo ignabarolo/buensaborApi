d5f06646e5c90a0484fd3428edfa8fe7469fa515
BetMartin <105375729+BetMartin@users.noreply.github.com>
2025-06-18 21:27:57 -0300

    Implementación de Anulacion de Factura

--- src/main/java/com/utn/buensaborApi/services/Interfaces/FacturaService.java
+++ src/main/java/com/utn/buensaborApi/services/Interfaces/FacturaService.java
@@ -3,4 +3,8 @@ package com.utn.buensaborApi.services.Interfaces;
 import com.utn.buensaborApi.models.Factura;
 
 public interface FacturaService extends BaseService<Factura, Long>{
+    Factura findById(Long id) throws Exception;
+    Factura save(Factura factura) throws Exception;
+    Factura anularFactura(Long facturaId) throws Exception;
+    byte[] generarNotaCredito(Long facturaId) throws Exception;
 }
